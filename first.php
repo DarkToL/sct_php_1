c56b3024f0e1f19e134dfdf40a1342de7cdcf4a8
DarkToL <botinok30@gmail.com>
2021-12-23 20:31:39 +0400

    23.12.2021

---
+++ first.php
@@ -0,0 +1,24 @@
+<?php
+$name = 'Анатолий';
+$adprop = 'ул.Ленина, д.20е';
+$adreal = 'ул.Савушкина, д.16ж';
+$age = '27';
+$gender = 'Мужской'
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo  "Имя:".$name .", Адрес регистрации: ".$adprop.", Адрес проживания: ".$adreal." ,Возраст:".$age;
+?>
+</body>
+</html>
+
+
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 echo 1
-include "sct_php_1";
+gi
